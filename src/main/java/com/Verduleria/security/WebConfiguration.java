7f46ed0dd1aa1fe6a7ddca9aefb43e68ee459373
josel <josel@DESKTOP-Q1HMB8T>
2023-03-12 22:55:15 -0600

    Cambio en las configuraiciones de seguridad (INCOMPLETO)

--- src/main/java/com/Verduleria/security/WebConfiguration.java
+++ src/main/java/com/Verduleria/security/WebConfiguration.java
@@ -2,11 +2,52 @@
  * https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
  */
 package com.Verduleria.security;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 
+@Configuration
+@EnableWebSecurity
 public class WebConfiguration {
-
-   
+    
+    @Autowired
+    private DataSource dataSource;
+    
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+    
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())   
+                .build();
+    }
+    
+    
     
 }
