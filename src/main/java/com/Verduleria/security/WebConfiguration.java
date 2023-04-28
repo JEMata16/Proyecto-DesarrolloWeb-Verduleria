482ec51f49bc901156ffed506425e8813272f68b
Joselyn López Chacón <jlopez90679@ufide.ac.cr>
2023-04-27 23:28:42 -0600

    chore: Agregar referencias y cambios en diseño

--- src/main/java/com/Verduleria/security/WebConfiguration.java
+++ src/main/java/com/Verduleria/security/WebConfiguration.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -38,17 +39,19 @@ public class WebConfiguration {
                     .requestMatchers("/empleado/**",
                             "/homePage",
                             "/inicio",
-                                    "/empleado",
+                                    "/empleado**",
                                     "/producto/**","/ventas/**").hasRole("ADMIN")
                 )
                 .formLogin((form)->form
                     .loginPage("/")
-                        .defaultSuccessUrl("/ventas/cajas")
+                        .defaultSuccessUrl("/inicio")
                         .loginProcessingUrl("/")
                         .failureUrl("/register")
                     .permitAll()
                 )
-                .logout((logout)->logout
+                .logout(
+                        (logout)->logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll()
                 .permitAll());
         
         return http.build();
